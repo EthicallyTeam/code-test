afeb026aee12be25205ceaa88fa32acc0478f666
Eric P <56278025+CodeYut@users.noreply.github.com>
2022-08-16 22:28:39 -0700

    Update products.js

--- be/routes/products.js
+++ be/routes/products.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const products = require('../data/products');
 const data = require('../data/products');
 const router = express.Router();
+const filteredProducts = products.filter(products => products.characteristics.includes(req.params.characteristics));
 
-router.get('/', function(req, res, next) {
-  res.send('Gonna start here')
+router.get('/data/products', function(req, res, next) {
+  res.send(data)
 });
+
+router.get('/data/products/:characteristics', function(req, res, next) {
+  res.send(filteredProducts)
+});
+
+
 
 module.exports = router;
